aa45254765ae792e74c88daa8cd2e01819338e04
Ariya <57851464+StormingChaos@users.noreply.github.com>
2021-11-29 11:39:59 -0500

    Enemy Movement
    - Nav Mesh for enemies
    - some enemies patrol between points
    - some enemies stand still
    - all enemies will chase player when they see them (will change later so only melee enemies chase)

---
+++ CE4482 App 3/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform[] points;          // array of patrol points
+    public float walkSpeed = 1.5f;      // how fast the enemy will patrol between points
+    public float chaseSpeed = 2.5f;     // how fast the agent will chase the player
+    public float turnSpeed = 1000;      // how fast the agent can turn
+    private int destPoint = 0;          // currently selected destination point
+    private NavMeshAgent agent;         // reference to Nav mesh agent
+    private Transform player;           // reference to player position
+    private bool chase;                 // if this enemy is chasing the player or not
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //set up reference to navmeshagent
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        // for continuous movement between points
+        agent.autoBraking = false;
+        chase = false;
+        agent.speed = walkSpeed;
+        agent.angularSpeed = turnSpeed;
+
+        // begin patrolling
+        GoToNextPoint();
+    }
+
+    private void GoToNextPoint()
+    {
+        // returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // set agent to go to currently selected point
+        agent.destination = points[destPoint].position;
+
+        // set next point in array as destination
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // if this enemy has seen the player, chase them
+        if (chase)
+        {
+            agent.SetDestination(player.position);
+        }
+        else if (GetComponent<FieldOfView>().canSeePlayer)
+        {
+            chase = true;
+            agent.speed = chaseSpeed;
+        }
+        // else choose next destination point when agent gets close to the current one
+        else if (!chase && !agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GoToNextPoint();
+        }
+    }
+}
